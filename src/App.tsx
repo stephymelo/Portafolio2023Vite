6023378dad2de7fbf3b8008898c343ff9d9654bb
stephym <stephymelo99@gmail.com>
2024-02-28 11:09:42 -0500

    finished resources

--- src/App.tsx
+++ src/App.tsx
@@ -30,14 +30,28 @@ import userflowFK from './assets/Images/FinkargoProject/userFK.png';
 import roadmapFK from './assets/Images/FinkargoProject/planFK.png';
 import designBannerFK from './assets/Images/FinkargoProject/designBanner.png';
 import designSystemFK from './assets/Images/FinkargoProject/designSystem.png';
+import formsFK from './assets/Images/FinkargoProject/Base.png';
+import panelFK from './assets/Images/FinkargoProject/PanelFinkargo.png';
 
 //Eficacia
 import bannerEficacia from './assets/Images/EficaciaProject/BannerDesignEfipuntos.png';
 import bannerEficaciaThumb from './assets/Images/EficaciaProject/BannerThumbnailEfi.png';
+import roadmapEficacia from './assets/Images/EficaciaProject/roadmapEficacia.png';
+import userEficacia from './assets/Images/EficaciaProject/userEficacia.png';
+import appEficacia from './assets/Images/EficaciaProject/appEficacia.png';
+import mockupEficacia from './assets/Images/EficaciaProject/EfipuntosMockup.png';
+import logoEficacia from './assets/Images/EficaciaProject/logoEficacia.png';
+import userFlowEficacia from './assets/Images/EficaciaProject/useflowEficacia.png';
 
 //Verby
 import bannerVerby from './assets/Images/VerbyProject/BannerDesignVerby.png';
 import bannerVerbyThumb from './assets/Images/VerbyProject/BannerThumbnailVerby.png';
+import mapVerby from './assets/Images/VerbyProject/map.png';
+import userPersonaVerby from './assets/Images/VerbyProject/UserPerosnaVeryb.png';
+import iconsVerby from './assets/Images/VerbyProject/icons.png';
+import framesVerby from './assets/Images/VerbyProject/FramesVerby.png';
+import screensVerby from './assets/Images/VerbyProject/screens.png';
+import prototypeVerby from './assets/Images/VerbyProject/prototypeVerby.png';
 
 import { Loader } from './Components/Loader/Loader';
 import ScrollToTop from './Components/ScrollToTop/ScrollToTop';
@@ -67,14 +81,14 @@ function App() {
       tasks: 'Close began as discovery for alternative ways to communicate and interact within a digital classroom due to the COVID-19 pandemic in 2020',
       tasks2: 'The goal of the project was to engage students in a digital classroom and aument their social interaction with their professors and classmates',
       date:'August 2020',
-      listTitle1:'Title',
-      list1:'listed',
-      listTitle2:'Title',
-      list2:'listed',
-      listTitle3:'Title',
+      listTitle1:'In depth investigation',
+      list1:'To have a clear understanding of concepts, we read academic papers, articles and interviewed teachers specialized in the topic',
+      listTitle2:'A robust documentation',
+      list2:'This helped us have the research background for the project and documentated the design process including the results of the vali',
+      listTitle3:'Implemented a design process',
       list3:'listed',
-      listTitle4:'Title',
-      list4:'listed',
+      listTitle4:'Innovative funcionalities',
+      list4:'We integrated functionalities that would help the student concentrate in class and also have a social interaction similar to in-person classes.',
       bannerImg: bannerClose,
       bannerThumbnail: bannerCloseThumb,
       subtitle1: 'Investigation',
@@ -87,7 +101,7 @@ function App() {
       paragraph3: 'Designer',
       image3: wireframes,
       subtitle4: 'Results and key takeways',
-      paragraph4: 'In the exploration of new features, including markers, channels, and annotations, within the classroom setting, a direct positive impact on students emotional well-being was observed. These tools not only exhibited a low sense of obligation, indicating that students willingly embraced them, but also introduced a welcome change to the classes routine. The positive reception extended to improved communication, evident through favorable feedback on the utility of the proposed tools. This enhancement not only contributed to an overall improvement in the class experience but also fostered a heightened sense of comfort among students. The introduction of these new tools played a significant role in fostering increased socialization among peers, particularly those in close proximity, resulting in a more engaging and dynamic class environment during various activities. Users showed voluntary engagement with marker and note-taking features, emphasizing their perceived benefits. Additionally, there was intrinsic motivation and interest expressed in utilizing channels for enhanced interaction. Despite users familiarity with video conferencing apps, challenges arose in identifying and using the new functions, revealing visibility issues in the prototypes design, especially in tasks related to channels. Recognition challenges were noted, causing difficulty for participants in finding and utilizing new functions, such as turning on the microphone and camera within a channel. Recognizing areas for improvement, the dashboard design needs modification for clarity, especially in distinguishing between entering the course page and entering the course session. Increased spacing between dashboard elements is recommended to avoid overwhelming users. Condensing information through icons is suggested for enhanced usability, and color and icon usage in the logbook can differentiate markers, notes, and class tasks. Survey results indicated users motivation and perceived benefits of the prototype, with users not feeling compelled to complete tasks, aligning with the prototypes intrinsic fulfillment of its purpose. Some users suggested additional social features, such as a user profile page. In conclusion, while the prototype demonstrated positive impacts, there are clear areas for improvement in design, user interface, and functionality to enhance overall usability and user experience',
+      paragraph4: 'Exploring new classroom features like markers and channels positively impacted students emotional well-being, fostering engagement and improved communication. However, challenges in using these tools revealed design flaws, particularly in visibility and recognition. Recommendations include clearer dashboard design, icon-based information condensation, and differentiation in the logbook. While users appreciated the prototypes benefits, improvements in design and functionality are necessary for enhanced usability and experience.',
       image4: journeyclose,
       image5:finalsclose,
       image6: mockupfinal
@@ -113,10 +127,10 @@ function App() {
       bannerThumbnail: bannerFinkargoThumb,
       subtitle1: 'Look and feel',
       paragraph1: 'When I first joined the startup, they had one main product which was financing clients imports but later on wanted to add other products and services, thus it was important to define the users and re-organize the site map. Collaborating with a dedicated research and data team, we conducted user interviews and crafted detailed user personas. Subsequently, working closely with the project managers, we developed a comprehensive sitemap. Leveraging my understanding of the product, I took on the responsibility of reshaping the design and user flow for their flagship product—import financing. Simultaneously, we worked on a redesign of the look and feel and worked on the design system. This system, once implemented by our developers, aimed to enhance consistency and efficiency across the platform.',
-      image1: 'Designer',
+      image1: formsFK,
       subtitle2: 'Creating the design system',
       paragraph2: 'I led the re-design of the design system, identifying which component was a priority to tackle and bringing proposals to our weekly sessions where we worked together to define the component. I did the documentation which was very important to have to be able to explain to the front ends the correct usage, variants, sizes and colors of the components. This also helped start a guide for implementing responsiveness which is important for accessibility.',
-      image2: 'Designer',
+      image2: panelFK,
       subtitle3: 'Roadmap with my P.M.',
       paragraph3: 'Following the roadmap of the product and ideas of my P.M, I created the user flows and interfaces to achieve the goal. This required constant communication and discussions to clarify and refine the flows with my P.M. I also communicated which flows we should prioritize based on their complexity, priority and length so that they could be delivered at least within one sprint of anticipation. I validated the interfaces with my P.M and conducted validations with both internal and external users before passing the final designs to our devs.',
       image3: designBannerFK,
@@ -135,30 +149,30 @@ function App() {
       tasks: 'Efipuntos, a coupon rewards app for products that the company Eficacia represents. The goal was to cultivate and maintain customer fidelity but also to facilitate the successful launch of new products by targeting the most relevant buyer segments.',
       tasks2: 'The digital solution stands at the intersection of customer engagement and product promotion, creating a dynamic platform that mutually benefits both users and the represented brands.',
       date:'May 2021',
-      listTitle1:'Title',
-      list1:'listed',
-      listTitle2:'Title',
-      list2:'listed',
-      listTitle3:'Title',
-      list3:'listed',
-      listTitle4:'Title',
-      list4:'listed',
+      listTitle1:'Co-creation session with stakeholders',
+      list1:'Allowed us to have a clearer idea of what they envisioned and truly needed',
+      listTitle2:'Multi-disclipinary team',
+      list2:'We worked with web designers, researchers and engineers to create the functional prototype.',
+      listTitle3:'Field work with real shoppers in supermarkets',
+      list3:'Conducting small interviews, observation and polls to understand shoppers choices and ideal userflow of shopping',
+      listTitle4:'High-fidelity prototype with Eficacia database',
+      list4:'We integrated the database of the companies products to have real time coupons and promotions shown in the web app',
       bannerImg: bannerEficacia,
       bannerThumbnail: bannerEficaciaThumb,
-      subtitle1: 'Background',
-      paragraph1: 'Designer',
-      image1: 'Designer',
+      subtitle1: 'Initial analysis',
+      paragraph1: 'We received the project from Eficacia which were looking into a way to promote the products they represent and increase engagement through a technological solution.',
+      image1: roadmapEficacia,
       subtitle2: 'Investigation',
-      paragraph2: 'Designer',
-      image2: 'Designer',
+      paragraph2: 'We conducted observations and interviews with buyers inside of supermarkets to understand their buying habits and thought process. We also had co-creation sessions with stakeholders in Miro. ',
+      image2: userFlowEficacia,
       subtitle3: 'Prototype',
-      paragraph3: 'Designer',
-      image3: 'Designer',
+      paragraph3: 'After presenting the initial prototype to the stakeholders and gathering valuable feedback, we expanded upon the concept. The updated idea included not only earning rewards through the purchase of products represented by Eficacia but also through participation in special events designed to promote new launches. This added dimension enhances user engagement and offers additional avenues for gaining points and coupons. Finally, we delivered a high-fidelity prototype in React TS, including connections to a database to simulate earning and redeeming coupons with different accounts.',
+      image3: userEficacia,
       subtitle4: 'Results and key takeways',
-      paragraph4: 'Designer',
-      image4: 'Designer',
-      image5:'image',
-      image6:'image'
+      paragraph4: 'Working for the first with a real client and stakeholders was an interesting challenge and learning experience.',
+      image4: mockupEficacia,
+      image5:appEficacia,
+      image6:logoEficacia,
 
     },
     {
@@ -169,30 +183,30 @@ function App() {
       tasks: 'Verby, an educational tool to help university students learn technical english in their career to help them become competitive professionals.',
       tasks2: 'The tool, which is a digital platform including a glossary, quizzes on course material with writing, speaking and listening questions, and preparation for interviews.',
       date:'December 2022',
-      listTitle1:'Title',
-      list1:'listed',
-      listTitle2:'Title',
-      list2:'listed',
-      listTitle3:'Title',
-      list3:'listed',
-      listTitle4:'Title',
-      list4:'listed',
+      listTitle1:'Solo project where I was in the whole design process',
+      list1:'From researching, brainstorming, validating with users and programming the prototype',
+      listTitle2:'Journey mapping and service blueprint',
+      list2:'Mapping the users journey and building the service blueprint so that the project could be continued by the university.',
+      listTitle3:'Resources from of the university',
+      list3:'Since the university was interested in the project, I had many resources from English teachers, classroom material and the English department to create the product.',
+      listTitle4:'The importance of technical language in English',
+      list4:'With a globalized world, many tech professions are requiered to know English and it can be the deciding factor in an interview',
       bannerImg: bannerVerby,
       bannerThumbnail: bannerVerbyThumb,
-      subtitle1: 'Background',
+      subtitle1: 'Wireframes and concepts',
       paragraph1: 'For my thesis project, I aimed to develop an educational tool for university students at Icesi University in Cali, Colombia. The project focused on the English Department, particularly investigating the implementation of Project E. This initiative involves transforming core classes across various majors to deliver their content in English. My research involved a comprehensive exploration of the stakeholders and actors involved to define the projects limits, which would be Technical language in English. I also investigated different teaching methodologies and existing tools.',
-      image1: 'Designer',
+      image1: mapVerby,
       subtitle2: 'Investigation',
       paragraph2: 'I used the Content and Language Integrated Learning (CLIL) methodology. Specifically designed for non-native English speakers, CLIL ensures a specialized approach to language acquisition. To enhance user engagement and encourage independent use, I incorporated elements of gamification. This not only motivates students to interact with the tool but also fosters a sense of competitiveness among peers, promoting a dynamic and effective learning environment.',
-      image2: 'Designer',
+      image2: userPersonaVerby,
       subtitle3: 'Prototype',
       paragraph3: 'The prototype was constructed after validations with Guerilla testing and Usability tests of wireframes with students in 5th semester of different majors. An experimental group later had to use the platform 3 times a week for a month for a certain class that had many English terminologies',
-      image3: 'Designer',
+      image3: iconsVerby,
       subtitle4: 'Results and conclusions',
       paragraph4: 'The students showed interest in the platform. They were evaluated on pronunciation, grammar and cohesiveness, where all of them showed a significant improvement. The stakeholders found the platform and the results positive and wished to continue the project on their own.',
-      image4: 'Designer',
-      image5:'image',
-      image6:'image'
+      image4: framesVerby,
+      image5:screensVerby,
+      image6:prototypeVerby,
 
     },
 
