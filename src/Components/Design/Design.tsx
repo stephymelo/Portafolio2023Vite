9d4ec46abb096b187faff363d2e913f06a8cc051
Stephanie Melo <stephanie@Stephanies-Laptop.local>
2025-08-29 11:39:04 -0400

    quick fix

--- src/Components/Design/Design.tsx
+++ src/Components/Design/Design.tsx
@@ -14,6 +14,7 @@ export const Design: React.FC<Design> = () => {
             title: 'Creating a user-friendly and social virtual classroom',
             description: 'Integrating social interactions in the digital medium for better communication and user motivation',
             image: 'https://www.indiewire.com/wp-content/uploads/2017/10/screen-shot-2017-10-10-at-6-57-53-pm.png',
+            imageMobile:'',
             path: '/design/1',
 
         },
@@ -23,6 +24,7 @@ export const Design: React.FC<Design> = () => {
             title: 'Design system for Fintech',
             description: 'Director',
             image: 'https://www.indiewire.com/wp-content/uploads/2017/10/screen-shot-2017-10-10-at-6-57-53-pm.png',
+             imageMobile:'',
             path: '/design/2',
 
         },
@@ -32,6 +34,7 @@ export const Design: React.FC<Design> = () => {
             title: 'Platform for learning technical language in English for non-natives',
             description: 'Gamification theories and micro-learning for a fun and easy way to learn terminology',
             image: 'https://www.indiewire.com/wp-content/uploads/2017/10/screen-shot-2017-10-10-at-6-57-53-pm.png',
+             imageMobile:'',
             path: '/design/2',
 
         }
